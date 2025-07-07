62b0eb0485a5e161d161a8c6ab5966a28c3ec2d7
rcame <rcameron4747@gmail.com>
2025-07-06 21:14:38 -0400

    Trying to get connection to MongoDB to work :)

--- digital-time-capsule/backend/app.py
+++ digital-time-capsule/backend/app.py
@@ -1,28 +1,37 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
-from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
+from flask_pymongo import PyMongo
+from flask_cors import CORS 
 
-load_dotenv()
+from datetime import datetime
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"])
 
-client = MongoClient(os.getenv("MONGODB_URI", "mongodb://localhost:27017"))
-db = client["timecapsule"]
-collection = db["entries"]
+# MongoDB config
+app.config["MONGO_URI"] = "mongodb+srv://rcameron4747:HAKvAlno540l9tb5@cluster0.knfwbcn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongo = PyMongo(app)
 
-@app.route('/api/entry', methods=['POST'])
-def create_entry():
-    data = request.json
-    collection.insert_one(data)
-    return jsonify({"message": "Entry saved!"})
+CORS(app)
 
-@app.route('/api/entries', methods=['GET'])
+@app.route("/api/entries", methods=["GET"])
 def get_entries():
-    entries = list(collection.find({}, {'_id': False}))
+    entries = list(mongo.db.entries.find())
+    for entry in entries:
+        entry["_id"] = str(entry["_id"])
     return jsonify(entries)
 
+@app.route("/api/entries", methods=["POST"])
+def add_entry():
+    data = request.get_json()
+    message = data.get("message")
+    open_date = data.get("openDate")
+    if not message or not open_date:
+        return jsonify({"error": "Missing fields"}), 400
+    mongo.db.entries.insert_one({
+        "message": message,
+        "openDate": open_date,
+        "createdAt": datetime.now()
+    })
+    return jsonify({"message": "Entry added"}), 201
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
