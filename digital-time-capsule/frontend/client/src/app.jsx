a1513011178957ea69675973648992b299787416
rcame <rcameron4747@gmail.com>
2025-08-13 09:54:36 -0400

    added logo; created Header.jsx

--- digital-time-capsule/frontend/client/src/app.jsx
+++ digital-time-capsule/frontend/client/src/app.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import CreateCapsule from "./components/CreateCapsule";
 import EntryList from "./components/EntryList";
+import Header from "./components/Header"
 
 export default function App() {
   const [reload, setReload] = useState(false);
 
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-4">Digital Time Capsule</h1>
+      <Header />
+      <main className="container mx-auto p-6">
       <CreateCapsule onAdd={() => setReload(!reload)} />
+      </main>
     </div>
   );
 }
---
+++ digital-time-capsule/frontend/client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="flex items-center p-4 bg-white shadow-md">
+      <img
+        src="https://storage.googleapis.com/digicap-components/Images/digicap_logo-removebg-preview.png"
+        alt="DigiCap Logo"
+        className="h-10 w-auto mr-3"
+      />
+      <h1 className="text-xl font-bold text-indigo-700"></h1>
+    </header>
+  );
+}
